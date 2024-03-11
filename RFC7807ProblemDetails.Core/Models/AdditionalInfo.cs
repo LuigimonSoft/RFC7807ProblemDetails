a8cc22c4198b71d64be02003410d8c764ac0a76c
Luis Carlos <almadejaguar@hotmail.com>
2024-03-11 00:40:23 -0400

    #3 Add data models

---
+++ RFC7807ProblemDetails.Core/Models/AdditionalInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC7807ProblemDetails.Core.Models
+{
+    public class AdditionalInfo
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+
+        public AdditionalInfo(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
